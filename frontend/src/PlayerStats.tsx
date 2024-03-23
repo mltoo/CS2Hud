a79f1ca355f10e56608fe87edab82b48342f4444
Alex Thomas <hello@alex-thomas.uk>
2024-03-23 00:28:50 +0000

    Stopped the player thing going nuts with <10 players with some 1am grade javascript

--- frontend/src/PlayerStats.tsx
+++ frontend/src/PlayerStats.tsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { PlayerBar } from './PlayerBar';
 
 export function PlayerStats(props) {
-    console.log(props);
-    const players: object[] = (Object.keys(props.data ?? {} as object) as string[]).map(steamID => { return { 'steamid': steamID, ...props.data[steamID] } }).sort(
-        (playerA: any, playerB: any) =>
-            ((playerA.observer_slot % 5) + (playerA.observer_slot / 10)) -
-            ((playerB.observer_slot % 5) + (playerB.observer_slot / 10)));
+    const players: object[] = (Object.keys(props.data ?? {} as object) as string[]).map(steamID => { return { 'steamid': steamID, ...props.data[steamID] } })
+    const mappedPlayers = {}
+    for (let player in players) {
+        players[player]['observer_slot'] != undefined && (mappedPlayers[players[player]['observer_slot']] = players[player]);
+    }
+    const arrangedPlayers : object[] = []
+    for (let i = 0; i < 10; i++) {
+        arrangedPlayers.push(mappedPlayers[Math.floor((i / 2) + (i%2)*5)] ?? {'observer_slot': Math.floor((i / 2) + (i%2)*4)});
+    }
+
 
     return <div className="grid grid-cols-2 grid-rows-5 w-screen absolute bottom-0 overflow-clip">
-        {players.map((player: any) => <PlayerBar key={player.observer_slot} data={player} round={props.round} roundPhase={props.roundPhase} roundDamage={props.roundDamage} isSpecced={props.currentSpec['steamid'] === player['steamid']}/>)
+        {arrangedPlayers.map((player: any) => player['name'] ? <PlayerBar key={player.observer_slot} data={player} round={props.round} roundPhase={props.roundPhase} roundDamage={props.roundDamage} isSpecced={props.currentSpec['steamid'] === player['steamid']}/> : <div/>)
         }
     </div>
 }
